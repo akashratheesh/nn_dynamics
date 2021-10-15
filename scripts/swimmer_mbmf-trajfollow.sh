216674012a5270f2e4b58f74708e8c974e3a6cdd
Akash Ratheeah <akashratheesh@gmail.com>
2021-10-15 14:47:34 -0600

    Chnaged chettah

--- scripts/swimmer_mbmf-trajfollow.sh
+++ scripts/swimmer_mbmf-trajfollow.sh
@@ -5,7 +5,7 @@
 #####################################
 
 #location of all saved trpo runs
-trpo_dir='/home/anagabandi/rllab/data/local/experiment/'
+trpo_dir='/rllab/data/local/experiment/'
 
 #specific to the run
 how_many_seeds=3
@@ -13,9 +13,9 @@ seeds=(0 70 100)
 num_workers_trpo=8 #how many cores to use
 
 #specific to the agent
-which_agent=4
-std_on_mlp_policy=0.5
-base_run_num=11 #used for filenames for saving
+which_agent=2
+std_on_mlp_policy=1.0
+base_run_num=1 #used for filenames for saving
 
 #####################################
 ## DO THE RUNS
@@ -31,7 +31,7 @@ do
 	echo $run_num
 	save_trpo_run_num=$(( 1 + $iter_num ))
 
-	python main.py --seed=$seed --run_num=$run_num --yaml_file='cheetah_forward'
+	python main.py --seed=$seed --run_num=$run_num --yaml_file='swimmer_forward'
 	python mbmf.py --run_num=$run_num --which_agent=$which_agent --std_on_mlp_policy=$std_on_mlp_policy
 	python trpo_run_mf.py --seed=$seed --save_trpo_run_num=$save_trpo_run_num --which_agent=$which_agent --num_workers_trpo=$num_workers_trpo
 
@@ -46,15 +46,15 @@ cd plotting
 
 if [ $how_many_seeds -eq 3 ]
 then
-python plot_mbmf.py --trpo_dir=$trpo_dir --std_on_mlp_policy=$std_on_mlp_policy --which_agent=$which_agent --run_nums 11 12 13 --seeds ${seeds[0]} ${seeds[1]} ${seeds[2]}
+python plot_mbmf.py --trpo_dir=$trpo_dir --std_on_mlp_policy=$std_on_mlp_policy --which_agent=$which_agent --run_nums 1 2 3 --seeds ${seeds[0]} ${seeds[1]} ${seeds[2]}
 fi
 
 if [ $how_many_seeds -eq 2 ]
 then
-python plot_mbmf.py --trpo_dir=$trpo_dir --std_on_mlp_policy=$std_on_mlp_policy --which_agent=$which_agent --run_nums 11 12 --seeds ${seeds[0]} ${seeds[1]}
+python plot_mbmf.py --trpo_dir=$trpo_dir --std_on_mlp_policy=$std_on_mlp_policy --which_agent=$which_agent --run_nums 1 2 --seeds ${seeds[0]} ${seeds[1]}
 fi
 
 if [ $how_many_seeds -eq 1 ]
 then
-python plot_mbmf.py --trpo_dir=$trpo_dir --std_on_mlp_policy=$std_on_mlp_policy --which_agent=$which_agent --run_nums 11 --seeds ${seeds[0]}
+python plot_mbmf.py --trpo_dir=$trpo_dir --std_on_mlp_policy=$std_on_mlp_policy --which_agent=$which_agent --run_nums 1 --seeds ${seeds[0]}
 fi
